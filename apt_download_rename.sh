2b9d9922a1caf8453aaaa73ace316ae21337fd61
kogcyc <kogcyc@gmail.com>
2024-02-12 10:30:01 -0800

    New file

---
+++ apt_download_rename.sh
@@ -0,0 +1,16 @@
+papirus-icon-theme inkscape git galculator python3-pip geeqie gpick vlc shotcut audacity povray
+
+rename 's/_.*\.deb$/.deb/' *.deb
+
+
+audacity.deb
+galculator.deb
+geeqie.deb
+git.deb
+gpick.deb
+inkscape.deb
+papirus-icon-theme.deb
+povray.deb
+python3-pip.deb
+shotcut.deb
+vlc.deb
