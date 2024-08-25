b65f8622b6d7bfccf050377a7a6359ee44062d53
kogcyc <kogcyc@gmail.com>
2024-08-25 08:32:33 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,4 @@
-find /home/matt -user matt \( -name "*.pdf" -o -name "*.svg" -o -name "*.SVG" -o -name "*.py" \) | grep -v "/home/matt/.local" | grep -v "/home/matt/snap" | grep -v "/home/matt/.cache" | rsync -R --files-from=- / /media/matt/HITACHI230/xxx
+
+sudo chown -R matt:matt /media/matt/TERRA/green1
+
+sudo find /home/matt -user matt \( -name "*.pdf" -o -name "*.svg" -o -name "*.SVG" -o -name "*.py" -o -name "*.odt" \) -not \( -path "/home/matt/.local/*" -o -path "/home/matt/snap/*" -o -path "/home/matt/.cache/*" \) | rsync -R --files-from=- / /media/matt/TERRA/green1
