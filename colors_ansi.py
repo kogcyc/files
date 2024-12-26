8673ebd700ea2a49132418b5b8328d0ebacbee8e
kogcyc <kogcyc@gmail.com>
2024-12-26 05:58:16 -0800

    Update colors_ansi.py

--- colors_ansi.py
+++ colors_ansi.py
@@ -1,16 +1,16 @@
-# ANSI escape codes for colors
-colors = {
-    "bright_red": "\033[91m",
-    "yellow": "\033[93m",
-    "cyan": "\033[96m",
-    "green": "\033[92m",
-    "magenta": "\033[95m",
-    "reset": "\033[0m",  # Reset to default color
+# ANSI escape codes for ANSI
+ANSI = {
+    "r": "\033[91m",
+    "y": "\033[93m",
+    "c": "\033[96m",
+    "g": "\033[92m",
+    "m": "\033[95m",
+    "r": "\033[0m",  # Reset to default color
 }
 
-# Print text in different colors
-print(f"{colors['bright_red']}This is bright red text!{colors['reset']}")
-print(f"{colors['yellow']}This is yellow text!{colors['reset']}")
-print(f"{colors['cyan']}This is cyan text!{colors['reset']}")
-print(f"{colors['green']}This is green text!{colors['reset']}")
-print(f"{colors['magenta']}This is magenta text!{colors['reset']}")
+# Print text in different ANSI
+print(f"{ANSI['r']}This is bright red text!{ANSI['r']}")
+print(f"{ANSI['y']}This is yellow text!{ANSI['r']}")
+print(f"{ANSI['c']}This is cyan text!{ANSI['r']}")
+print(f"{ANSI['g']}This is green text!{ANSI['r']}")
+print(f"{ANSI['m']}This is magenta text!{ANSI['r']}")
